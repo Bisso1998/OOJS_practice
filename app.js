98b345e2d84b149518636d9f897b4b2609425c30
biswarup <bis.banerjee.bb@gmail.com>
2017-10-07 02:37:32 +0530

    added delete feature

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 var Course = {};
-
-$( document ).ready(function() {
+$(document ).ready(function() {
     console.log( "ready!" );
     var i=0;
     var courseName = "NULL", courseCode = "NULL";
+    deleteRec =     function () {
+         console.log("Deleted!");
+       }
     // makes the DOM ready for manipulation
     $('.addData > input').addClass('courseData');
     console.log("i am out!!!");
+
     $( '#showAll' ).click(function() {
       var allInputs = $( "input.courseData" );
       // storing all the inputs in an array allInputs by the class name courseData
-      console.log(allInputs[0].value);
       // extracting the values indexwise from the array
-      console.log(allInputs[1].value);
-      console.log(allInputs);
       courseName = allInputs[0].value;
       courseCode = allInputs[1].value;
       Course[i]  = {
         name : courseName,
         code : courseCode,
       };
-      $( "#addCards" ).append( "Added " + Course[i].name + " with course code "+ Course[i].code + "<br>");
+      $( "#addCards" ).append( "<p id=" +i+  "> Added " + Course[i].name + " with course code "+ Course[i].code + "  <button id="+ i +  " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "<br> </p>");
       i++;
     });
     // adding courseData class to all input fields under addData class
 });
+function deleteMe(i) {
+  delete Course[i];
+  $('#'+i).remove();
+
+}
