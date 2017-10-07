108b27a1de1879949215f346d340d47e43117f5f
biswarup <bis.banerjee.bb@gmail.com>
2017-10-07 13:51:47 +0530

    removed bugs

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ $(document ).ready(function() {
     deleteRec =     function () {
          console.log("Deleted!");
        }
-    // makes the DOM ready for manipulation
     $('.addData > input').addClass('courseData');
     console.log("i am out!!!");
 
     $( '#showAll' ).click(function() {
       var allInputs = $( "input.courseData" );
-      // storing all the inputs in an array allInputs by the class name courseData
-      // extracting the values indexwise from the array
       courseName = allInputs[0].value;
       courseCode = allInputs[1].value;
       Course[i]  = {
@@ -23,7 +20,6 @@ $(document ).ready(function() {
       $( "#addCards" ).append( "<p id=" +i+"p"+  "> Added " + Course[i].name + " with course code "+ Course[i].code + "  <button id="+ i +  " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "  <button id="+ i +  " "+'onclick="editMe('+ i +')"' +'> Edit </button>'+  "<br> </p>");
       i++;
     });
-    // adding courseData class to all input fields under addData class
 });
 function deleteMe(i) {
   delete Course[i];
@@ -32,7 +28,5 @@ function deleteMe(i) {
 function editMe(i) {
   var name = prompt("Enter name....");
   Course[i].name = name;
-  $( "#"+i+"p" ).html( "<p id=" +i+ "p"+ "> Added " + Course[i].name + " with course code "+ Course[i].code + "  <button id="+ i + "p" + " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "  <button id="+ i + "p" +  " "+'onClick="editeMe('+ i +')"' +'> Edit </button>'+  "<br> </p>");
-
-  $('#'+i).remove();
+  $( "#"+i+"p" ).html( "<p id=" +i+ "p"+ "> Added " + Course[i].name + " with course code "+ Course[i].code + "  <button id="+ i + "p" + " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "  <button id="+ i + "p" +  " "+'onClick="editMe('+ i +')"' +'> Edit </button>'+  "<br> </p>");
 }
