5f69610cb4297ae6b6c27776aff0b868f7797d66
biswarup <bis.banerjee.bb@gmail.com>
2017-10-12 06:21:48 +0530

    adding search feature

--- app.js
+++ app.js
@@ -14,33 +14,15 @@ $(document ).ready(function() {
         code : courseCode,
       };
       count++;
-      $( "#addCards" ).append( "<p id=" +i+"p"+  "> Added " + Course[i].name + " with course code "+ Course[i].code + "  <button id="+ i +  " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "  <button id="+ i +  " "+'onclick="editMe('+ i +')"' +'> Edit </button>'+  "<br> </p>");
+      $( "#addCards" ).append( "<li><p id=" +i+"p"+  "> Added " + Course[i].name + " with course code "+ Course[i].code + "  <button id="+ i +  " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "  <button id="+ i +  " "+'onclick="editMe('+ i +')"' +'> Edit </button>'+  "<br> </p></li>");
       i++;
     });
-    $('#sortUs').click(function() {
-      console.log("Hello is selected");
-      var code = [],k=0;
-      for(var i=0;i<count;i++)
-      {
-        code[i] = Course[i].code;
-      }
-      console.log("code is: "+ code);
-      // code = code.filter(function( element ) {
-      //   return element !== undefined;
-      // });
-      code  = code.sort();
-      console.log("sorted: " + code);
-      // for(var i=0;i<count;i++)
-      // {
-      //
-      // }
-      // $( "#sortedCards" ).append( "<p id=" +i+"p"+  "> Added " + Course[i].name + " with course code "+ Course[i].code + "  <button id="+ i +  " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "  <button id="+ i +  " "+'onclick="editMe('+ i +')"' +'> Edit </button>'+  "<br> </p>");
 
-    });
 });
 function deleteMe(i) {
     // Course.splice(i,1);
   delete Course[i];
+  // Course.splice(0,1);
   // Course.splice(i,1);
   $('#'+i+"p").remove();
   count--;
@@ -48,5 +30,24 @@ function deleteMe(i) {
 function editMe(i) {
   var name = prompt("Enter name....");
   Course[i].name = name;
-  $( "#"+i+"p" ).html( "<p id=" +i+ "p"+ "> Added " + Course[i].name + " with course code "+ Course[i].code + "  <button id="+ i + "p" + " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "  <button id="+ i + "p" +  " "+'onClick="editMe('+ i +')"' +'> Edit </button>'+  "<br> </p>");
+  $( "#"+i+"p" ).html( "<li><p id=" +i+ "p"+ "> Added " + "<b>"+ Course[i].name + "</b>" + " with course code "+ Course[i].code + "  <button id="+ i + "p" + " "+'onClick="deleteMe('+ i +')"' +'> Delete </button>'   + "  <button id="+ i + "p" +  " "+'onClick="editMe('+ i +')"' +'> Edit </button>'+  "<br> </p><li>");
+}
+
+function searchAll() {
+  console.log("Input keyup");
+  var input, filter, ul, li, p, i;
+  input = document.getElementById('inputTake');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("addCards");
+  li = ul.getElementsByTagName('li');
+  for (i = 0; i < li.length; i++) {
+      p = li[i].getElementsByTagName("p")[0];
+      console.log("val of p is "+ p.innerHTML);
+      // a =  li[i].getElementById('sarchMe');
+      if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
 }
